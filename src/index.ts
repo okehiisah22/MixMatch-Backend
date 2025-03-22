70475151a0c403cb0d87620024c21ef686d55d82
Emeka-web-dev <emekarexchukwu@gmail.com>
2025-03-22 09:49:00 +0100

    feature: add passport middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import session from "cookie-session"
+
+import './config/passport.config';
+import passport from "passport";
 import logger from './config/logger';
 import { loggingHandler } from './middleware/pinoHttp';
 import { routeError } from './middleware/routeError';
@@ -20,9 +24,22 @@ app.use(loggingHandler);
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: false }));
 
+app.use(session({
+  name: 'session',
+  keys: [process.env.SESSION_SECRET as string],
+  maxAge: 24 * 60 * 60 * 1000,
+  secure: process.env.NODE_ENV === "production",
+  httpOnly: true,
+  sameSite: "lax",
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/health', (req, res) => {
   res.status(200).json({ greeting: 'Hello World! Mixmatch' });
 });
+
+// app.use("/api/auth", )
 
 app.use(routeError);
 
