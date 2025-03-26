85bc751f1baa13a5925560f8f53d0aec6d8274c3
Shukazuby <shukazuby@gmail.com>
2025-03-26 15:12:31 +0100

    Implemented Message CRUD endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 
 import authRouter from "./auth.routes";
 import djProfile_Router from "./DjProfile.router";
+import messageRouter from "./message.routes";
 
 const router = express.Router();
 export default (): express.Router => {
   authRouter(router);
   djProfile_Router(router);
+  messageRouter(router);
   return router;
 };
