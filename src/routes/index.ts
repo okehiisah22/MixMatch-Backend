49686c9b436bbc03643149033fdcfaff63a9f265
shamoo53 <aliyushamsu16@gmail.com>
2025-03-26 11:19:05 +0100

    Implementation of CRUD Operations for Events Model

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 
 import authRouter from "./auth.routes";
 import djProfile_Router from "./DjProfile.router";
+import eventRoutes from "./eventRoutes";
 
 const router = express.Router();
 export default (): express.Router => {
   authRouter(router);
   djProfile_Router(router);
+  router.use("/events", eventRoutes);
   return router;
 };
