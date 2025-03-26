2fede685bc6cd08452423f63316f435353ea6965
MaryammAli <maryamaliyu09159@gmail.com>
2025-03-26 12:22:21 +0100

    implementation of crud operations for bookings model

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 
 import authRouter from "./auth.routes";
 import djProfile_Router from "./DjProfile.router";
+import bookingRoutes from "./booking.routes";
 
 const router = express.Router();
 export default (): express.Router => {
   authRouter(router);
   djProfile_Router(router);
+  router.use("/bookings", bookingRoutes);
   return router;
 };
